99bbcd4255ef6688b46c6549f458c89a98974289
Ryan Vazquez <contact@ryan-vazquez.dev>
2021-01-09 14:43:35 -0600

    Add drag

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -8,10 +8,25 @@ public class Plane : MonoBehaviour {
     [SerializeField]
     float throttleSpeed;
 
+    [SerializeField]
+    AnimationCurve dragForward;
+    [SerializeField]
+    AnimationCurve dragBack;
+    [SerializeField]
+    AnimationCurve dragLeft;
+    [SerializeField]
+    AnimationCurve dragRight;
+    [SerializeField]
+    AnimationCurve dragTop;
+    [SerializeField]
+    AnimationCurve dragBottom;
+
     float throttleInput;
 
     public Rigidbody Rigidbody { get; private set; }
     public float Throttle { get; private set; }
+    public Vector3 Velocity { get; private set; }
+    public Vector3 LocalVelocity { get; private set; }
 
     void Start() {
         Rigidbody = GetComponent<Rigidbody>();
@@ -30,17 +45,45 @@ public class Plane : MonoBehaviour {
         Throttle = Utilities.MoveTo(Throttle, target, throttleSpeed * Mathf.Abs(throttleInput), dt);
     }
 
+    void CalculateState() {
+        Velocity = Rigidbody.velocity;
+        LocalVelocity = Quaternion.Inverse(Rigidbody.rotation) * Velocity;  //transform world velocity into local space
+    }
+
     void UpdateThrust() {
         Rigidbody.AddRelativeForce(Throttle * maxThrust * Vector3.forward);
     }
 
+    void UpdateDrag() {
+        var lv = LocalVelocity;
+        var lv2 = lv.sqrMagnitude;  //velocity squared
+
+        //calculate coefficient of drag depending on direction on velocity
+        var coefficient = Utilities.Scale6(
+            lv.normalized,
+            dragRight.Evaluate(lv.x), dragLeft.Evaluate(-lv.x),
+            dragTop.Evaluate(lv.y), dragBottom.Evaluate(-lv.y),
+            dragForward.Evaluate(lv.z), dragBack.Evaluate(-lv.z)
+        );
+
+        var drag = coefficient.magnitude * lv2 * -lv.normalized;    //drag is opposite direction of velocity
+
+        Rigidbody.AddRelativeForce(drag);
+    }
+
     void FixedUpdate() {
         float dt = Time.fixedDeltaTime;
+
+        CalculateState();
 
         //handle user input
         UpdateThrottle(dt);
 
         //apply updates
         UpdateThrust();
+
+        CalculateState();
+
+        UpdateDrag();
     }
 }
--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -8,4 +8,34 @@ public static class Utilities {
         float delta = Mathf.Clamp(diff, -speed * deltaTime, speed * deltaTime);
         return Mathf.Clamp(value + delta, min, max);
     }
+
+    //similar to Vector3.Scale, but has separate factor negative values on each axis
+    public static Vector3 Scale6(
+        Vector3 value,
+        float posX, float negX,
+        float posY, float negY,
+        float posZ, float negZ
+    ) {
+        Vector3 result = value;
+
+        if (result.x > 0) {
+            result.x *= posX;
+        } else if (result.x < 0) {
+            result.x *= negX;
+        }
+
+        if (result.y > 0) {
+            result.y *= posY;
+        } else if (result.y < 0) {
+            result.y *= negY;
+        }
+
+        if (result.z > 0) {
+            result.z *= posZ;
+        } else if (result.z < 0) {
+            result.z *= negZ;
+        }
+
+        return result;
+    }
 }
