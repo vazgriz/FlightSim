bb4067e76483e896fef3069c5d79b4903cdde12b
Ryan Vazquez <contact@ryan-vazquez.dev>
2021-01-08 21:45:49 -0600

    Create input system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    public void SetThrottleInput(InputAction.CallbackContext context) {
+
+    }
+}
