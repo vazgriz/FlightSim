268279090a9df8fc435b9f09ee836bac16c21d21
Ryan Vazquez <contact@ryan-vazquez.dev>
2021-02-20 16:27:19 -0600

    Add Target and Missile

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+    [SerializeField]
+    float lifetime;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float trackingAngle;
+    [SerializeField]
+    float damage;
+    [SerializeField]
+    float turningG;
+
+    Target target;
+    new Rigidbody rigidbody;
+
+    public void Launch(Target target) {
+        this.target = target;
+
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        rigidbody.velocity = rigidbody.rotation * new Vector3(0, 0, speed);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+    [SerializeField]
+    new string name;
+}
