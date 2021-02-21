277134fccbbc761076b20d7be841bc75b1803969
Ryan Vazquez <contact@ryan-vazquez.dev>
2021-02-20 18:29:05 -0600

    Add missile collision logic

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -13,17 +13,64 @@ public class Missile : MonoBehaviour {
     float damage;
     [SerializeField]
     float turningG;
+    [SerializeField]
+    LayerMask collisionMask;
+    [SerializeField]
+    new MeshRenderer renderer;
 
+    Plane owner;
     Target target;
     new Rigidbody rigidbody;
+    bool exploded;
+    Vector3 lastPosition;
+    float timer;
 
-    public void Launch(Target target) {
+    public void Launch(Plane owner, Target target) {
+        this.owner = owner;
         this.target = target;
 
         rigidbody = GetComponent<Rigidbody>();
+
+        lastPosition = rigidbody.position;
+        timer = lifetime;
+    }
+
+    void Explode() {
+        if (exploded) return;
+
+        timer = lifetime;
+        rigidbody.isKinematic = true;
+        renderer.enabled = false;
+        exploded = true;
+    }
+
+    void CheckCollision() {
+        var currentPosition = rigidbody.position;
+        var error = currentPosition - lastPosition;
+        var ray = new Ray(lastPosition, error.normalized);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, error.magnitude, collisionMask.value)) {
+            Plane other = hit.collider.gameObject.GetComponent<Plane>();
+
+            if (other == null || other != owner) {
+                Explode();
+            }
+        }
+
+        lastPosition = currentPosition;
     }
 
     void FixedUpdate() {
+        timer = Mathf.Max(0, Time.fixedDeltaTime);
+
+        if (timer == 0) {
+            Destroy(gameObject);
+        }
+
+        if (exploded) return;
+
+        CheckCollision();
         rigidbody.velocity = rigidbody.rotation * new Vector3(0, 0, speed);
     }
 }
--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -416,7 +416,7 @@ public class Plane : MonoBehaviour {
         var hardpoint = hardpoints[index];
         var missileGO = Instantiate(missilePrefab, hardpoint.position, hardpoint.rotation);
         var missile = missileGO.GetComponent<Missile>();
-        missile.Launch(null);
+        missile.Launch(this, null);
     }
 
     void UpdateWeapons(float dt) {
